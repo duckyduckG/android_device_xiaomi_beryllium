04b4d758f69e107c1edc45db81d166803399c3e1
duckyduck <duckyduckg65@gmail.com>
2025-02-12 05:07:19 +0000

    Revert "beryllium: Awaken-ify"
    This reverts commit 41d345f69c8fba84c4eb358f73fbeb3dffb17215.

--- device.mk
+++ device.mk
@@ -208,6 +208,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
@@ -250,6 +254,10 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_PACKAGES += \
     BerylliumCarrierConfigOverlay \
     BerylliumFrameworksOverlay \
@@ -307,6 +315,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+
+# PocketMode
+PRODUCT_PACKAGES += \
+    XiaomiPocketMode
 
 # Power
 PRODUCT_PACKAGES += \
