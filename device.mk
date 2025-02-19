b6a5c54d4a7b91a9bc4f8a6ef1ad1da000d8d48e
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-02-19 10:59:33 +0500

    sdm845-common: Move to common Xiaomi light AIDL Change-Id: I00494cd392d19abd97cd65aeb4ec4bd9c9b11352

--- device.mk
+++ device.mk
@@ -203,7 +203,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_beryllium
+    android.hardware.light-service.xiaomi
 
 # Lineage Health
 PRODUCT_PACKAGES += \
