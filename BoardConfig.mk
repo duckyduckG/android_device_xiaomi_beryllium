8147f2b545f96a90e92e9807dba6db00a0160dd9
duckyduckG <duckyduckg65@gmail.com>
2025-02-19 14:12:24 +0500

    test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,7 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
